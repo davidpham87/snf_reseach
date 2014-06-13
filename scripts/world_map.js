35797f1e958566d145938c9350bf65b2831d4def
David Pham <davidpham87@gmail.com>
2014-06-13 14:58:55 +0200

    Fixing bugs

--- scripts/world_map.js
+++ scripts/world_map.js
@@ -73,12 +73,7 @@ function createBrush(){
       d3.select('#worldDate').text(new_date.toString().substring(4,15)); // Update da
     }    
 
-    // clearInterval(myPlotInterval);
-    // myPlotInterval = setInterval(plotHeart, Math.floor(100-value+1));
   }
-
-  // slider.select(".background")
-  //     .attr("height", height);
 
   svg_slider.append("g")
     .attr("class", "x axis")
@@ -135,12 +130,15 @@ function createBrush(){
       var new_date = new Date(value, month, 1, 0, 0, 0, 0);
       return new_date;
     }
-    
-    //Hack to stop the animation
-    if (brush.extent()[0] !== features.value) {          
+
+    function stop_animation(value){
       clearInterval(refreshIntervalId);
-      dte.date = toDateFromValue(features.value);
+      dte.date = toDateFromValue(value);
       createWorldMap();
+    }
+    //Hack to stop the animation
+    if (brush.extent()[0] !== features.value || features.value > 2014) {
+      stop_animation(features.value);
       return;
     }
     
@@ -149,35 +147,26 @@ function createBrush(){
     var value = features.value;
     var new_date = toDateFromValue(value);
 
-    // console.log(features.value);
-    // console.log(brush.extent());
-
     brush.extent([value, value])
     handle.attr("cx", x(value));       
-    // Here for doing the animation
-    // console.log(new_date);
     updateMap(new_date);
     d3.select('#worldDate').text(new_date.toString().substring(4,15)); // Update date
-
-    if (features.value > 2014){
-      console.log(features.value);
-      clearInterval(refreshIntervalId);
-      dte.date = new_date;
-      createWorldMap();
-    }
   }
 
   function animate_map(){ 
 
-    var value = brush.extent()[0]; // First value of the year # Should work on scale.domain maybe
+    // First value of the year # Should work on scale.domain maybe
+    var value = brush.extent()[0]; 
+
     animation_features = {'value': value};  
-    clearInterval(refreshIntervalId);
+
+    clearInterval(refreshIntervalId); // Stop previous animation
+
     refreshIntervalId = setInterval(
       function() {
         update_animation(animation_features);
       }, 100);
-    
-    /* later */  
+
   }
 }
 
@@ -401,132 +390,9 @@ function createKeyLegend(){
 
 createKeyLegend();
 
-//Function kept to do the tooltip
-function putCityVoronoi(error, data){
-  
-  var centroid = d3.geo.centroid;
 
-  data.forEach(function(d){
-    d.longitude = +d.longitude;
-    d.latitude = +d.latitude;
-    d.geojson = {type: 'Point', 
-                 coordinates: [d.latitude, d.longitude]};
-  });
-  
-  //Color scale
-  var color = d3.scale.quantile()
-    .range(d3.range(10).map(function(i) { j = i; return "q" + j + "-10"; }))
-    .domain([0, 1]);
-
-  //Voronoi tessleation
-  var voronoi = d3.geom.voronoi()
-    .clipExtent([[0,0], [width, height]]);
-
-  //Add a circle in the middle of the municipalities
-  var pathCentroid = function(d, i){
-    return path.centroid(d.geojson);
-  }
-
-  
-  // Voronoi tesslation
-
-  //Creates a voronois tesselation
-  function polygon(d) {
-    return "M" + d.join("L") + "Z";
-  }
-  
-  positions = data.map(function(d){
-    return pathCentroid(d);
-  })
-
-  polygons = d3.geom.voronoi(positions);
-
-  var voronoiArea = svg.append("g")
-    .attr("class", "municipality-voronoi")
-  // .attr("transform", 'translate(-' + translate_width_constant + ',0)')
-    .selectAll("path")
-    .data(polygons, polygon) //apply the polygon function to the dat polygon
-    .enter().append("path")
-    .attr("d", polygon)
-    .attr("stroke-width", "0")
-    .on("click", updateSlider);
-  
-  var tooltip = d3.select("body")
-	  .append("div")
-    .attr("class", "d3-tip worldMapTip")
-	  .style("position", "absolute")
-	  .style("z-index", "10")
-	  .style("visibility", "hidden");
-  
-  function htmlInsideTooltipFn(d, i){
-    d = data[i];
-    htmlText = "<div align='center'>";
-    htmlText += '<font color="red">' + d.location + "</font> ";
-    htmlText += '   (' + d.visitedDate + ')';
-    htmlText += '</div>';
-    // Add something here (Such as descriptions or date of tripe)  
-    return htmlText;
-  }
-  addToolTip(voronoiArea, tooltip, htmlInsideTooltipFn);
-  
-  //Add Circle to cities
-  var cities = svg.append("g")
-    .selectAll("circle")
-    .data(data)
-    .enter().append("circle")
-    .attr("class", "city")
-  //    .attr('r', function(d) {return 0.22*(d.bubbleSize+1);})
-    .attr('r', city_radius)
-    .attr("cx", function(d) { return pathCentroid(d)[0]; })
-    .attr("cy", function(d) { return pathCentroid(d)[1]; })
-    .on("click", updateSlider);
-
-  addToolTip(cities, tooltip, htmlInsideTooltipFn);
-  
-  var this_location;
-  function updateSlider(d, i){
-    
-    d = data[i];
-    if (this_location == d.location){
-      return;
-    }
-    console.log(this_location);
-    this_location = d.location;
-    var newSlides = updatePictures(d.location, d.nPictures);
-    d3.select('#pictureSlider')
-      .remove();
-    
-    d3.select('#sliderDiv')
-      .append('div')
-      .attr('class', 'flexslider')      
-      .attr('id', 'pictureSlider')
-      .style('width', '640px')
-      .style('height', '480px')
-      .html(newSlides);
-    
-    $('.flexslider').flexslider({
-      animation: "slide",
-      slideshowSpeed: 3000,
-    });
-  }
-
-  function updatePictures(location, nPictures){
-    var newPict = function(n){            
-      return '<li> \n  <img src="data/pictures/' + 
-        location + '/' + (n < 10 ? '0' + n : n) + 
-        '.jpg" /> \n </li> \n';
-    };
-    
-    htmlText = '<ul class="slides"> \n';
-    for (var i=1; i <= nPictures; ++i){
-      htmlText += newPict(i);
-    }
-    htmlText += '</ul> \n';
-    //console.log(htmlText);
-    return htmlText;
-  }
-
-}
+//==============================================================================
+// Tooltip functions
 
 function htmlInsideTooltipFn(d, i){
 
@@ -542,8 +408,6 @@ function htmlInsideTooltipFn(d, i){
 }
 
 
-
-
 function addToolTip(g, tooltip, htmlInsideTooltipFn){
   g.on("mouseover", function(d, i){
     tooltip
@@ -553,6 +417,9 @@ function addToolTip(g, tooltip, htmlInsideTooltipFn){
 	  .on("mousemove", function(){return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");})
 	  .on("mouseout", function(){return tooltip.style("visibility", "hidden");})
 }
+
+//==============================================================================
+// Misc
 
 function get_translate(s){
   if (s == undefined){
@@ -573,7 +440,8 @@ function get_scale(s){
   return s;
 }
 
+//===============================================================================
 
-d3.select(self.frameElement).style("height", height + "px");
+d3.select(self.frameElement).style("height", height + "px");  // I don't know what it is, but we need...
 
 
