67642a9769d094b6227df46e11527aa07896f534
David Pham <davidpham87@gmail.com>
2014-06-13 14:48:24 +0200

    Fixing bugs

--- scripts/world_map.js
+++ scripts/world_map.js
@@ -1,10 +1,40 @@
 var width = 960,
-    height = 480,
-    height_slider = 80;
+  height = 480,
+  height_slider = 80;
+//===============================================================================
 
+var date_format = d3.time.format("%Y-%m-%d");
+var dte = {'date' : date_format.parse('2008-01-01')}; // First Date
+
+var data_ts;
+var data_nest;
+var world_data;
+var count_by_date = d3.map();
+var country_num_to_iso3 = d3.map(); // numcode to name of the country
+
+//===============================================================================
+// Color scaler
+
+function createQuantize(){
+
+  var quantize = d3.scale.quantize()
+    .domain([0, 6])
+    .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));  // Color
+
+  return quantize;
+}
 
 //===============================================================================
 // Brush 
+
+function createBrush(){
+
+  var animation_features = {'value': 2008}; // For animation
+  var refreshIntervalId; // Function for stopping the animation
+  
+  var play_button = d3.select('#worldMapButton') // Button to click to launch the animation
+    .on("click", animate_map);
+
   var svg_slider = d3.select('#worldMapSlider').append('g')
     .attr("transform",  "translate(" + 40 + "," + 40 + ")")
 
@@ -18,8 +48,6 @@ var width = 960,
     .extent([2008, 2008])
     .on("brush", updateBrush);
 
-  var myPlotInterval;
-
   var value = brush.extent()[0];
   var month = Math.floor((value - Math.floor(value))*100/12) + 1;
   var this_date;
@@ -29,17 +57,20 @@ var width = 960,
     var month = Math.floor((value - Math.floor(value))*100/12) + 1;
     var new_date = new Date(value, month, 1, 0, 0, 0, 0);
     
+    dte.date = new_date;
+
     if (d3.event.sourceEvent) { // not a programmatic event
       value = x.invert(d3.mouse(this)[0]);
       brush.extent([value, value]);
     }
-
+    
     handle.attr("cx", x(value));      
-        
+    
     // Here for doing the animation
     if (this_date != new_date){
       updateMap(new_date);
       this_date = new_date;
+      d3.select('#worldDate').text(new_date.toString().substring(4,15)); // Update da
     }    
 
     // clearInterval(myPlotInterval);
@@ -49,9 +80,9 @@ var width = 960,
   // slider.select(".background")
   //     .attr("height", height);
 
-   svg_slider.append("g")
+  svg_slider.append("g")
     .attr("class", "x axis")
-    .attr("transform", "translate(0 " + 30 + ")")
+    .attr("transform", "translate(0 " + 0 + ")")
     .attr("z-index", "10")
     .attr("fill", "black")
     .call(d3.svg.axis()
@@ -61,17 +92,22 @@ var width = 960,
           .tickFormat(function(d) { return d; })
           .tickSize(0)
           .tickPadding(4)
-          // .attr("dy", "-1em")
+          //.attr("dy", "1em")
           // .text("Speed of creation")
          )
     .append("text")
     .attr("x", x(2008))
-    .attr("dy", "-2em")
+    .attr("dy", "-1.5em")
     .style("text-anchor", "end")
     .text("Date")
     .select(".domain")
-    .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-    .attr("class", "halo");
+    .select(function() { 
+      return this.parentNode.appendChild(this.cloneNode(true)); 
+    })
+    .attr("class", "halo")
+    .on("click", function(){
+      clearInterval(refreshIntervalId);
+    });;
 
   var slider = svg_slider.append("g")
     .attr("class", "slider")
@@ -85,138 +121,287 @@ var width = 960,
 
   var handle = slider.append("circle")
     .attr("class", "handle")
-    .attr("transform", "translate(0," + 30  + ")")
-    .attr("r", 9);
+    .attr("transform", "translate(0," + 0  + ")")
+    .attr("r", 9)
+    .on("click", function(){
+      clearInterval(refreshIntervalId);
+    });
+  //=============================================================================
+  // Animation on play button
+  function update_animation(features){
+    
+    function toDateFromValue(value){  
+      var month = Math.floor((value - Math.floor(value))*100/12) + 1;
+      var new_date = new Date(value, month, 1, 0, 0, 0, 0);
+      return new_date;
+    }
+    
+    //Hack to stop the animation
+    if (brush.extent()[0] !== features.value) {          
+      clearInterval(refreshIntervalId);
+      dte.date = toDateFromValue(features.value);
+      createWorldMap();
+      return;
+    }
+    
+    features.value += 1/12;
+
+    var value = features.value;
+    var new_date = toDateFromValue(value);
+
+    // console.log(features.value);
+    // console.log(brush.extent());
+
+    brush.extent([value, value])
+    handle.attr("cx", x(value));       
+    // Here for doing the animation
+    // console.log(new_date);
+    updateMap(new_date);
+    d3.select('#worldDate').text(new_date.toString().substring(4,15)); // Update date
+
+    if (features.value > 2014){
+      console.log(features.value);
+      clearInterval(refreshIntervalId);
+      dte.date = new_date;
+      createWorldMap();
+    }
+  }
+
+  function animate_map(){ 
+
+    var value = brush.extent()[0]; // First value of the year # Should work on scale.domain maybe
+    animation_features = {'value': value};  
+    clearInterval(refreshIntervalId);
+    refreshIntervalId = setInterval(
+      function() {
+        update_animation(animation_features);
+      }, 100);
+    
+    /* later */  
+  }
+}
+
+createBrush();
 
 //===============================================================================
-//World map
+// World map
 
-//d3.geo.azimuthalEqualArea()
-var projection = d3.geo.mercator() 
-  .scale((width + 1) / 1.65 / Math.PI)
-  .translate([width / 2 - 25, height / 2 + 80])
-  .precision(.1);
+function createWorldMap(){
 
-var path = d3.geo.path()
-  .projection(projection);
+  //d3.geo.azimuthalEqualArea()
+  var projection = d3.geo.mercator() 
+    .scale((width + 1) / 1.65 / Math.PI)
+    .translate([width / 2 - 25, height / 2 + 80])
+    .precision(.1);
 
-var graticule = d3.geo.graticule();
+  var path = d3.geo.path()
+    .projection(projection);
 
-var quantize = d3.scale.quantize()
-  .domain([0, 6])
-  .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));  // Color
+  var graticule = d3.geo.graticule();
 
-// Zoom hack
-var zoom = d3.behavior.zoom()
-  .translate([0, 0])
-  .scale(1)
-  .scaleExtent([0.5, 10])
-  .on("zoom", zoomed);
-
-d3.select('#worldMap')
-  .attr("class", "overlay")
-  .attr("width", width)
-  .attr("height", height)
-  .call(zoom);
-
-var svg =  d3.select('#worldMap').append("g");
-
-var city_radius = 5;
-
-function zoomed() {
-  var d3_scale = d3.event.scale;
-  svg.attr("transform", "translate(" + d3.event.translate + ") scale(" + d3_scale + ")");
-  svg.selectAll(".city")
-    .attr("r", city_radius/Math.sqrt(d3_scale))
-    .style("stroke-width", 2/Math.sqrt(d3_scale) + 'px');
-}
-// End of Zoom
-
-svg.append("path")
-  .datum(graticule)
-  .attr("class", "graticule")
-  .attr("d", path);
-
-var count_by_date = d3.map();
-var date_format = d3.time.format("%Y-%m-%d");
-var data_ts;
-var data_nest;
-var dte = date_format.parse('2009-01-01');
-var world_data;
-// function(error, data){data_ts = data;})
-
-queue()
-  .defer(d3.json, "data/world-50m.json")
-  .defer(d3.csv, "data/dat_ly_ts.csv", function(d){
-    return {
-      N: +d.N,
-      date: date_format.parse(d.date),
-      id: d.numcode,
-      base_disciplin: d.base_discplin //Mistake/Typo in the csv file...
-    }
-  })
-  .await(createMap);
-
-// d3.csv('data/location.csv', putCityVoronoi)
+  var quantize = d3.scale.quantize()
+    .domain([0, 6])
+    .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));  // Color
 
 
-function createMap(error, world, data) {
-  data_ts = data;
-  world_data = world;
-  // console.log(data[0]);
+
+  // Zoom hack
+  var zoom = d3.behavior.zoom()
+    .scaleExtent([0.5, 10])
+    .on("zoom", zoomed);
+
+  d3.select('#worldMap')
+    .attr("class", "overlay")
+    .attr("width", width)
+    .attr("height", height)
+    .call(zoom);
   
-  var nest = d3.nest()
-    .key(function(d) {return d.date})
-    .key(function(d) {return d.id})
-    .rollup(function(leaves) {
-      return d3.sum(leaves, function(d) {return d.N});
+  var svg =  d3.select('#worldMap').append("g");
+
+  // var city_radius = 5;
+
+  function zoomed() {
+    var d3_scale = d3.event.scale;
+    svg.attr("transform", "translate(" + d3.event.translate + ") scale(" + d3_scale + ")");
+    // svg.selectAll(".city")
+    //   .attr("r", city_radius/Math.sqrt(d3_scale))
+    //   .style("stroke-width", 2/Math.sqrt(d3_scale) + 'px');
+  }
+  // End of Zoom
+
+  svg.append("path")
+    .datum(graticule)
+    .attr("class", "graticule")
+    .attr("d", path);
+
+
+
+  queue()
+    .defer(d3.json, "data/world-110m.json")
+    .defer(d3.csv, "data/dat_ly_ts.csv", function(d){
+      return {
+        N: +d.N,
+        date: date_format.parse(d.date),
+        id: d.numcode,
+        base_disciplin: d.base_discplin //Mistake/Typo in the csv file...
+      }
     })
-    .entries(data);
+    .defer(d3.csv, "data/country.csv", function(d){
+      country_num_to_iso3.set(d.numcode, d.name);
+    })
+    .await(createMap);
+  
+  // In order to refresh the map so that the move in the world map stays fluid.
+  var svg_old_worldmap = d3.select('#worldMap_g');
+  svg_old_worldmap
+    .transition()
+    .duration(1000)
+    .ease(Math.sqrt)
+    .style('opacity', 1e-16)
+    .remove();
 
-  console.log(nest[23].values[0]);
-  data_nest = nest;
+  svg.attr("id", 'worldMap_g');
 
-  for (var i = 0; i < data_nest.length; ++i){
-    var count_by_country = d3.map();
-    var country_count = data_nest[i].values;    
+  function createMap(error, world, data) {
+    data_ts = data;
+    world_data = world;
+    // console.log(data[0]);
+    
+    data_nest = d3.nest()
+      .key(function(d) {return d.date})
+      .key(function(d) {return d.id})
+      .rollup(function(leaves) {
+        return d3.sum(leaves, function(d) {return d.N});
+      })
+      .entries(data);
 
-    for (var j=0; j < country_count.length;  ++j){
-      count_by_country.set(country_count[j].key, country_count[j].values);
+    for (var i=0; i<data_nest.length; ++i){
+      var count_by_country = d3.map();
+      var country_count = data_nest[i].values;    
+
+      for (var j=0; j<country_count.length;  ++j){
+        count_by_country.set(country_count[j].key, country_count[j].values);
+      }
+
+      count_by_date.set(data_nest[i].key, count_by_country);
     }
 
-    count_by_date.set(data_nest[i].key, count_by_country);
-  }
-    
-  svg.selectAll(".country")  
-    .data(topojson.feature(world, world.objects.countries).features)
-  .enter().insert("path", ".graticule")
-    .attr("class", function(d) {
-      console.log(d);
-      var n = count_by_date.get(dte).get(String(d.id));
-      n = n !== undefined ? n : 0;
-      console.log(n);
-      return quantize(Math.log(n+1));})
-    .attr("d", path);
-}
+    if (!count_by_date.has(dte.date)){
+      return;
+    }
 
+    
+    var countries = svg.selectAll(".country")  
+      .data(topojson.feature(world, world.objects.countries).features)
+      .enter().insert("path", ".graticule")
+      .attr("class", function(d) {
+        // console.log(d);
+        var n = count_by_date.get(dte.date).get(String(d.id));
+        n = n !== undefined ? n : 0;
+        // console.log(n);
+        return quantize(Math.log(n+1));})
+      .attr("d", path);
+
+    // var borders = svg.insert("path", ".graticule")
+    //   .datum(topojson.mesh(world_data, world_data.objects.countries, function(a, b) { return a !== b; }))
+    //   .attr("class", "boundary")
+    //   .attr("d", path);
+
+    // Tooltip for the countries
+    var tooltip = d3.select("body")
+	    .append("div")
+      .attr("class", "d3-tip worldMapTip")
+      .attr('id', 'tooltip_worldmap')
+	    .style("position", "absolute")
+	    .style("z-index", "10")
+	    .style("visibility", "hidden");
+    
+    console.log('AFter tooltip');
+    addToolTip(countries, tooltip, htmlInsideTooltipFn);
+  }
+}
 
 function updateMap(new_date){
 
-  svg.selectAll(".country")  
+  var projection = d3.geo.mercator() 
+    .scale((width + 1) / 1.65 / Math.PI)
+    .translate([width / 2 - 25, height / 2 + 80])
+    .precision(.1);
+
+  var path = d3.geo.path()
+    .projection(projection);
+
+  var quantize = d3.scale.quantize()
+    .domain([0, 6])
+    .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));  // Color
+
+  var svg =  d3.select('#worldMap_g');
+  
+  if (!count_by_date.has(new_date)){
+    return;
+  }
+
+  var countries = svg.selectAll(".country")  
     .data(topojson.feature(world_data, world_data.objects.countries).features)
-  .enter().insert("path", ".graticule")
+    .enter().insert("path", ".graticule")
     .attr("class", function(d) {
       var n = count_by_date.get(new_date).get(String(d.id));
       n = n !== undefined ? n : 0;
-      console.log(n);
+      // console.log(n);
       return quantize(Math.log(n+1));})
     .attr("d", path);
+  
+  var tooltip = d3.select('#tooltip_worldmap');  
+  addToolTip(countries, tooltip, htmlInsideTooltipFn);
 
 }
 
+createWorldMap();
 
-// Put dots on the map for the location from a csv file 
-// location, long, lat, sizeBubble, nPictures
+// The axis mapping color and the number
+function createKeyLegend(){
+  
+  var quantize = createQuantize();
+
+  var x = d3.scale.linear()
+    .domain([0, 6])
+    .range([0, width*0.4]);
+
+  var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
+    .tickSize(9)
+    .tickFormat(d3.format(".1f"));
+
+  var key = d3.select('#worldMapKey').append("g")
+    .attr("class", "key Oranges")
+    .attr("transform", "translate(10, 10)");
+  //      .attr("transform", "translate(" + (width - 40) + "," + (height - 30) + ")rotate(-90)");
+  
+  key.append("rect")
+    .attr("x", -10)
+    .attr("y", -10)
+    .attr("width", width*0.44)
+    .attr("height", 40)
+    .style("fill", "grey")
+    .style("fill-opacity", 0.2);
+
+  key.selectAll("g")
+    .data(d3.pairs(x.ticks(9)))
+    .enter().append("rect")
+    .attr("class", function(d) { return quantize(d[0]); })
+    .attr("height", 8)
+    .attr("x", function(d) { return x(d[0]); })
+    .attr("width", function(d) { return x(d[1]) - x(d[0]); });
+  
+  key.call(xAxis);
+  key.selectAll("text")
+    .text(function(d) {return Math.round(Math.exp(d));});
+}
+
+createKeyLegend();
+
+//Function kept to do the tooltip
 function putCityVoronoi(error, data){
   
   var centroid = d3.geo.centroid;
@@ -290,7 +475,7 @@ function putCityVoronoi(error, data){
     .data(data)
     .enter().append("circle")
     .attr("class", "city")
-//    .attr('r', function(d) {return 0.22*(d.bubbleSize+1);})
+  //    .attr('r', function(d) {return 0.22*(d.bubbleSize+1);})
     .attr('r', city_radius)
     .attr("cx", function(d) { return pathCentroid(d)[0]; })
     .attr("cy", function(d) { return pathCentroid(d)[1]; })
@@ -343,6 +528,22 @@ function putCityVoronoi(error, data){
 
 }
 
+function htmlInsideTooltipFn(d, i){
+
+  var n = count_by_date.get(dte.date).get(String(d.id));
+  n = n !== undefined ? n : 0;
+
+  htmlText = "<div align='center'>";
+  htmlText += '<font color="red">' + country_num_to_iso3.get(d.id) + "</font> ";
+  htmlText += '   (' + n + ')';
+  htmlText += '</div>';
+  // Add something here (Such as descriptions or date of tripe)  
+  return htmlText;
+}
+
+
+
+
 function addToolTip(g, tooltip, htmlInsideTooltipFn){
   g.on("mouseover", function(d, i){
     tooltip
@@ -353,4 +554,26 @@ function addToolTip(g, tooltip, htmlInsideTooltipFn){
 	  .on("mouseout", function(){return tooltip.style("visibility", "hidden");})
 }
 
+function get_translate(s){
+  if (s == undefined){
+    return [0, 0]
+  }
+  var s = s.split(' ')[0];
+  s = s.substring(11, s.length-1);
+  var res = s.split(',').map(function(d) {return +d;});
+  return res;
+}
+
+function get_scale(s){
+  if (s == undefined){
+    return 1;
+  }
+  var s = s.split(' ')[1];
+  s = +s.substring(6, s.length-1);
+  return s;
+}
+
+
 d3.select(self.frameElement).style("height", height + "px");
+
+
