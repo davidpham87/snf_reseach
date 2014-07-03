4c19de19c45a9604eff5295c84a24183ff218df5
David Pham <davidpham87@gmail.com>
2014-07-04 00:43:21 +0200

    Fixing bugs

--- snf_apps/server.R
+++ snf_apps/server.R
@@ -13,4 +13,18 @@ shinyServer(function(input, output) {
     dat.l <- dat.lyb[J(base_disc, input$year), list(N = sum(N)), by='iso3']
       print(world_map_void + geom_map(data = dat.l, aes(fill = log(N)), map = world.ggmap))
   })
+
+  output$myTs <- renderPlot({
+    base_disc <- numeric(0)
+    if (input$social.sci) base_disc <- 1
+    if (input$base.sci) base_disc <- c(base_disc, 2)
+    if (input$bio.med) base_disc <- c(base_disc, 3)
+    if (length(base_disc) == 0) return()
+
+    dat.l <- dat.lyb[J(base_disc), list(N = sum(N)), by='iso3,project_year']
+    setkey(dat.l, iso3)
+    dat.l <- dat.l[J(dat.l[, min(log(N+1)), by='iso3'][V1 > 1.5, iso3])]
+
+    print(ggplot(dat.l, aes(project_year, log(N), color=iso3)) + geom_line())
+  })
 })
--- snf_apps/ui.R
+++ snf_apps/ui.R
@@ -13,30 +13,36 @@ shinyUI(fluidPage(
     a("http://davidpham87.github.io/snf_research/", href = "http://davidpham87.github.io/snf_research/"),
     br(),
     p("You just need to use press the play button to launch the animation or check the boxes to have subset the data.")
-  ),
+    ),
   
-  # Copy the line below to make a slider bar
+                                        # Copy the line below to make a slider bar
   fluidRow(
     column(6, 
            sliderInput("year", label = h3("Year"), min = 2008, max = 2014, 
-              value = 2008, format = '####', 
-              animate=animationOptions(interval=2000, loop=TRUE))
-    ),
+                       value = 2008, format = '####', 
+                       animate=animationOptions(interval=2000, loop=TRUE))
+           ),
     column(6, 
            h3('Disciplin'),
            checkboxInput('social.sci', 'Social Sciences', T),
            checkboxInput('base.sci', 'Base Sciences', T),
            checkboxInput('bio.med', 'Biology/Med', T)
            )
-  ),
+    ),
   hr(), 
   fluidRow(
     column(width = 3,   verbatimTextOutput("value"))
-  ),
+    ),
   
-  fluidRow(
+  fluidRow(      
     column(width=12,
-           plotOutput("myChart", height = 720, width = 1200)
+      tabsetPanel(
+        tabPanel("World Map", 
+                 plotOutput("myChart", height = 720, width = 1200)),
+         tabPanel("Time Series",
+                 plotOutput('myTs', height = 720, width = 1200))       
+        )
+      )
     )
   )
-))
+)        
